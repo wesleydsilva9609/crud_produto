bce880279f932149110e065dca32f1bfc1c7c2c1
Wesley D. Silva <wesleydsilva96@gmail.com>
2025-10-02 15:06:56 -0300

    iniciando projeto

---
+++ src/test/java/br/com/alura/crudProduto/CrudProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.crudProduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
